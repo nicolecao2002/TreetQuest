55a92874c7c4c58f41c0555cbdf139f50db44a3f
nicolecao2002 <nicole.cao@utexas.edu>
2023-06-27 23:49:38 +0800

    finish setting up the login page

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -3,7 +3,10 @@ import './Login.css'
 import video from '../../LoginAssets/video.mp4'
 import logo from '../../LoginAssets/logo3.png'
 import {Link, NavLink} from 'react-router-dom'
-import {FaUserShield} from 'react-icons/fa'
+import { FaUserShield } from 'react-icons/fa'
+import { BsFillShieldLockFill } from 'react-icons/bs'
+import { AiOutlineSwapRight } from 'react-icons/ai'
+
 const Login = () =>
 {
   return (
@@ -28,15 +31,34 @@ const Login = () =>
                       <img src={ logo } alt="Logo Image" />
                       <h3>Welcome Back!</h3>
                   </div>
-                  <form action="" className='form gird'></form>
-                  <span>Login status will go here</span>
-                  <div className="inputDiv">
-                      <label htmlFor='username'>Username</label>
-                      <div className="inpu tflex">
-                          <FaUserShield className='icon' />
-                          <input type='text' id='username' placeholder='Enter Username'/>
-                      </div>
-                  </div>
+                  
+                  <form action="" className='form gird'>
+                  
+                    <span>Login status will go here</span>
+                    
+                    <div className="inputDiv">
+                        <label htmlFor='username'>Username</label>
+                        <div className="input flex">
+                            <FaUserShield className='icon' />
+                            <input type='text' id='username' placeholder='Enter Username'/>
+                        </div>
+                    </div>
+                    
+                    <div className="inputDiv">
+                        <label htmlFor='password'>Password</label>
+                        <div className="input flex">
+                            <BsFillShieldLockFill className='icon' />
+                            <input type='password' id='password' placeholder='Enter Password'/>
+                        </div>
+                    </div>
+
+                    <button type='submit' className='btn flex'>
+                        <span>Login</span>
+                        <AiOutlineSwapRight className='icon'/>           
+                    </button>
+                      <span className='forgotPassword'>Forgot Password? <a href="">Click Here</a>
+                      </span>
+                  </form>
               </div>
         </div>     
     </div>
