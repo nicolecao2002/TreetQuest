6ba937f1bde7877d220a01c17543d1fe0e2b8d57
nicolecao2002 <nicole.cao@utexas.edu>
2023-06-27 23:20:22 +0800

    add username input and icon

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Login.css'
 import video from '../../LoginAssets/video.mp4'
+import logo from '../../LoginAssets/logo3.png'
 import {Link, NavLink} from 'react-router-dom'
-const Login = () => {
+import {FaUserShield} from 'react-icons/fa'
+const Login = () =>
+{
   return (
     <div className='loginPage flex'>
           <div className='container flex'>
@@ -19,6 +22,21 @@ const Login = () => {
                           <button className='btn'>Sign Up</button>
                       </Link>
                     </div>
+              </div>
+              <div className='formDiv flex'>
+                  <div className='headerDiv'>
+                      <img src={ logo } alt="Logo Image" />
+                      <h3>Welcome Back!</h3>
+                  </div>
+                  <form action="" className='form gird'></form>
+                  <span>Login status will go here</span>
+                  <div className="inputDiv">
+                      <label htmlFor='username'>Username</label>
+                      <div className="inpu tflex">
+                          <FaUserShield className='icon' />
+                          <input type='text' id='username' placeholder='Enter Username'/>
+                      </div>
+                  </div>
               </div>
         </div>     
     </div>
