3901bc08000ab27899aba808cfa26cfac6019b39
nicolecao2002 <nicole.cao@utexas.edu>
2023-07-31 19:30:40 +0800

    add the css file and the error message

--- client/src/Components/ToDoList/TodolistMain.jsx
+++ client/src/Components/ToDoList/TodolistMain.jsx
@@ -1,12 +1,15 @@
+// TodolistMain.jsx
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import '../../App.css';
+import './ToDoList.css';
 
 const TodolistMain = () => {
   const [newTaskName, setNewTaskName] = useState('');
   const [tasks, setTasks] = useState([]);
   const [userId, setUserId] = useState(null);
   const [userFetched, setUserFetched] = useState(false);
+  const [selectedLevel, setSelectedLevel] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     fetchUserId();
@@ -14,7 +17,9 @@ const TodolistMain = () => {
 
   const fetchUserId = async () => {
     try {
-      const response = await axios.get('http://localhost:3002/dashboard', { withCredentials: true });
+      const response = await axios.get('http://localhost:3002/dashboard', {
+        withCredentials: true,
+      });
       setUserId(response.data.userId);
       console.log('User ID:', response.data.userId);
       setUserFetched(true);
@@ -43,17 +48,27 @@ const TodolistMain = () => {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    if (!newTaskName.trim()) return;
+    if (!newTaskName.trim()) {
+      setErrorMessage('Task name cannot be empty.');
+      return;
+    }
+
+    if (!selectedLevel) {
+      setErrorMessage('Please select a task level.');
+      return;
+    }
 
     try {
       const response = await axios.post('http://localhost:3002/todolistMain', {
         task_name: newTaskName,
-        task_level: 'normal',
+        task_level: selectedLevel,
         status: 'todo',
         task_creator_id: userId,
       });
       setTasks([...tasks, response.data]);
       setNewTaskName('');
+      // Clear the error message if there was no error
+      setErrorMessage('');
     } catch (error) {
       console.error('Error adding task:', error);
     }
@@ -85,36 +100,55 @@ const TodolistMain = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="new_item_form">
-        <div className="form-row">
-          <label htmlFor="task">New Task</label>
-          <input value={newTaskName} onChange={(e) => setNewTaskName(e.target.value)} type="text" id="task" />
-              </div>
-            
-        <button className="btn">Add</button>
-      </form>
-
-      <h1 className="header">Task List</h1>
-      <ul className="list">
-        {tasks.length === 0 && <div>No Tasks</div>}
-        {tasks.map((task) => (
-          <li key={task.task_id}>
-            <label>
-              <input
-                type="checkbox"
-                checked={task.status === 'completed'}
-                onChange={() => toggleTask(task.task_id, task.status)}
-              />
-              {task.task_name}
-            </label>
-            <button onClick={() => deleteTask(task.task_id)} className="btn btn-danger">
-              Delete
+    <div className="page_container">
+      <div className="form-container">
+        <form onSubmit={handleSubmit} className="new-item-form">
+          <h2>New Task</h2>
+          <div className="form-row">
+            <span>
+              <input value={newTaskName} onChange={(e) => setNewTaskName(e.target.value)} type="text" id="task" />
+            </span>
+          </div>
+          <div className="btnFormContainer">
+            <button className="levelSbtn" onClick={() => setSelectedLevel('small')}>
+              Small
             </button>
-          </li>
-        ))}
-      </ul>
-      <a href="/dashboard">Return</a>
+            <button className="levelMbtn" onClick={() => setSelectedLevel('medium')}>
+              Medium
+            </button>
+            <button className="levelLbtn" onClick={() => setSelectedLevel('large')}>
+              Large
+            </button>
+          </div>
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <button className="add-btn">Add</button>
+        </form>
+      </div>
+
+      <div className="list-container">
+        <h1 className="header">Task List</h1>
+        <ul className="list">
+          {tasks.length === 0 && <div>No Tasks</div>}
+          {tasks.map((task) => (
+            <li key={task.task_id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={task.status === 'completed'}
+                  onChange={() => toggleTask(task.task_id, task.status)}
+                />
+                {task.task_name}
+              </label>
+              <button onClick={() => deleteTask(task.task_id)} className="delete-btn">
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+        <a href="/dashboard" className="return-link">
+          Return
+        </a>
+      </div>
     </div>
   );
 };
